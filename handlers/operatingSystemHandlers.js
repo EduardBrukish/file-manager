41d367a6e308b063811b0a2b2d5e18ddb00f2854
Eduard Brukish <eduardbrukish@gmail.com>
2024-01-28 21:45:56 +0200

    feat(file-manager): add operating system logic

---
+++ handlers/operatingSystemHandlers.js
@@ -0,0 +1,62 @@
+import { homedir, userInfo, cpus, EOL, arch } from 'os'
+
+const printUserEOL = () => {
+  console.log(`Your EOL: ${JSON.stringify(EOL)}`)
+}
+
+const printUserMachineCpuData = () => {
+  const userMachineCpuData = cpus()
+  const cpusCoresNumber = userMachineCpuData.length
+  const cpusData = userMachineCpuData.map((cpu) => ({
+    model: cpu.model,
+    speed: `${(cpu.speed / 1000).toFixed(2)} GHz`
+  }))
+
+  console.log(`Your CPU number: \n`, cpusCoresNumber)
+  console.log(`Your CPU data: \n`, cpusData)
+}
+
+const printUserHomeDirectory = () => {
+  const userHomeDirectory = homedir()
+  console.log(`Your home directory: ${userHomeDirectory}`)
+}
+
+const printUserName = () => {
+  try {
+    const currentUserInfo = userInfo()
+    const {username} = currentUserInfo
+    console.log(`User name: ${username.username}`)
+  } catch (e) {
+    console.log('Error occurred for Operating System operator')
+  }
+}
+
+const printUserMachineCpuArchitecture = () => {
+  const cpuArchitecture = arch()
+  console.log(`Your CPU architecture: ${cpuArchitecture}`)
+}
+
+export const handleUserOperationSystem = (osArguments) => {
+  const [flag] = osArguments || []
+
+  switch (flag) {
+    case '--EOL': 
+      printUserEOL()
+      break
+    case '--cpus': 
+      printUserMachineCpuData()
+      break
+    case '--homedir': 
+      printUserHomeDirectory()
+      break
+    case '--username': 
+      printUserName()
+      break
+    case '--architecture': 
+      printUserMachineCpuArchitecture()
+      break
+    default:
+      console.log('Sorry, you used invalid argument for Operating System cmdlet')
+  } 
+
+}
