fee0c6c915a711a55b963d3da53642f4111ef0fc
Eduard Brukish <eduardbrukish@gmail.com>
2024-01-30 22:52:07 +0200

    feat(file-manager): update basic operation logic

--- handlers/basicOperationHandlers.js
+++ handlers/basicOperationHandlers.js
@@ -1,39 +1,132 @@
-import { rm } from 'fs/promises'
+import { rm, writeFile, readFile, rename } from 'fs/promises'
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
 import { join } from 'path'
 
-const deleteFile = async () => {
-  const fileDirectory = getDirname(import.meta.url)
-  const filePathToDelete = join(fileDirectory, 'files', 'fileToRemove.txt')
-
+const deleteFile = async (currentDirectoryPath, deleteFileArguments) => {
   try {
-      await rm(filePathToDelete)
-  } catch (e) {
-      throw new Error('Failed to remove file')
+    const [fileNameToDelete] = deleteFileArguments
+    const filePathToDelete = join(currentDirectoryPath, fileNameToDelete)
+
+    await rm(filePathToDelete)
+  } catch {
+    console.log('Error occurred during delete operation. Check please command params')
   }
 }
 
-export const handleUserBasicOperation = (operationType, basicOperationArguments) => {
-  switch (operationType) {
-    case 'cat': 
-      console.log(basicOperationArguments)
-      break
-    case 'add': 
-    console.log(basicOperationArguments)
-      break
-    case 'rn': 
-    console.log(basicOperationArguments)
-      break
-    case 'cp': 
-    console.log(basicOperationArguments)
-      break
-    case 'mv': 
-    console.log(basicOperationArguments)
-      break
-    case 'rm': 
-    console.log(basicOperationArguments)
-      break
-    default:
-      console.log('Sorry, you used invalid argument for Basic Operation cmdlet')
-  } 
+const createFile = async (currentDirectoryPath, createFileArguments) => {
+  try {
+    const [fileName] = createFileArguments
 
+    if(!fileName) {
+      console.log('Invalid file name')
+      return
+    }
+
+    const filePathToCreate = join(currentDirectoryPath, fileName)
+  
+    await writeFile(filePathToCreate, '', { flag: 'wx'})
+  } catch {
+    console.log('Error during file creation')
+  }
+}
+
+const copyFile = async (currentDirectoryPath, copyFileArguments) => {
+  try {
+    const [fileNameToCopy, destinationFileName] = copyFileArguments
+
+    if(!fileNameToCopy || !destinationFileName) {
+      console.log('Invalid arguments to copy file')
+      return
+    }
+
+    const filePathToCopy = join(currentDirectoryPath, fileNameToCopy)
+    const destinationFilePath = join(currentDirectoryPath, destinationFileName)
+
+    const readStream = createReadStream(filePathToCopy, { encoding: 'utf8' })
+    const writeStream = createWriteStream(destinationFilePath)
+
+    await pipeline(readStream, writeStream)
+  } catch {
+    console.log('Invalid arguments to copy file')
+  }
+}
+
+const readFileByPath = async (currentDirectoryPath, readFileArguments) => {
+  try {
+    const [fileNameToRead] = readFileArguments
+    const filePathToRead = join(currentDirectoryPath, fileNameToRead)
+
+    const fileData = await new Promise((res, rej) => {
+      let readStream = createReadStream(filePathToRead, 'utf8')
+      let chunk = ''
+
+      readStream.on('data', (data) => {
+          chunk = chunk + data
+      })
+
+      readStream.on('close', () => {
+          res(chunk)
+      })
+
+      readStream.on('error', (e) => {
+          rej(e)
+      })
+
+    })
+
+    process.stdout.write(fileData)
+  } catch {
+    console.log('Error occurred during read file operation. Check please command params')
+  }
+};
+
+const renameFile = async (currentDirectoryPath, renameFileArguments) => {
+  try {
+    const [filePathToRename, newFileName] = renameFileArguments
+    const fileToRenamePath = join(currentDirectoryPath, filePathToRename)
+    const properFileNamePath = join(currentDirectoryPath, newFileName)
+
+    await rename(fileToRenamePath, properFileNamePath)
+  } catch {
+    console.log('Error occurred during read file operation. Check please command params')
+  }
+};
+
+const moveFile = async (currentDirectoryPath, moveFileArguments) => {
+  try {
+    await copyFile(currentDirectoryPath, moveFileArguments)
+    await deleteFile(currentDirectoryPath, moveFileArguments)
+  } catch {
+    console.log('Error occurred during move file operation. Check please command params')
+  }  
+}
+
+export const handleUserBasicOperation = async (operationType, basicOperationArguments, currentDirectoryPath) => {
+  try {
+    switch (operationType) {
+      case 'cat': 
+        await readFileByPath(currentDirectoryPath, basicOperationArguments)
+        break
+      case 'add': 
+        await createFile(currentDirectoryPath, basicOperationArguments)
+        break
+      case 'rn': 
+        await renameFile(currentDirectoryPath, basicOperationArguments)
+        break
+      case 'cp': 
+        await copyFile(currentDirectoryPath, basicOperationArguments)
+        break
+      case 'mv': 
+        await moveFile(currentDirectoryPath, basicOperationArguments)
+        break
+      case 'rm': 
+        await deleteFile(currentDirectoryPath, basicOperationArguments)
+        break
+      default:
+        console.log('Sorry, you used invalid argument for Basic Operation cmdlet')
+    } 
+  } catch {
+    console.log('Error occurred for Basic Operation cmdlet')
+  }
 }
--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import { handleUserOperationSystem } from './handlers/operatingSystemHandlers.js
 import { handleUserBasicOperation } from './handlers/basicOperationHandlers.js'
 import { BASIC_OPERATIONS } from './utils/constants.js'
 
+// ToDO delete it after move logic
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
 const getUserName = (commandLineArguments) => {
   const [ userNameArgument ] = commandLineArguments.splice(2)
   const userName = userNameArgument && userNameArgument.replace('--', '')
@@ -14,24 +18,31 @@ const getUserName = (commandLineArguments) => {
 const handleUserInput = async (userInput, currentDirectory) => {
   const [operationType, ...args] = userInput.trim().split(/\s+/g)
 
-  switch (operationType) {
-    case 'os':
-      handleUserOperationSystem(args)
-      break
-    case BASIC_OPERATIONS[operationType]:
-      await handleUserBasicOperation(operationType, args, currentDirectory)
-      break
-    case '.exit':
-      process.exit(1);
-      break
-    default:
-      console.log()
+  try {
+    switch (operationType) {
+      case 'os':
+        handleUserOperationSystem(args)
+        break
+      case BASIC_OPERATIONS[operationType]:
+        await handleUserBasicOperation(operationType, args, currentDirectory)
+        break
+      case '.exit':
+        process.exit(1);
+        break
+      default:
+        console.log('Check please your input')
+    }
+  } catch {
+    console.log('Check please your input')
   }
 }
 
 const initFileManagerApp = async () => {
   const userName = getUserName(argv)
-  let currentDirectory = homedir()
+
+// ToDO delete it after move logic
+  const filePath = fileURLToPath(import.meta.url)
+  const dirPath = dirname(filePath)
 
   if (!userName) {
     const errorMessage = `Invalid username argument.\n Please start File Manager with command: "npm run start -- --username=your_username" \n` 
@@ -39,6 +50,7 @@ const initFileManagerApp = async () => {
   }
 
   console.log(`Welcome to the File Manager, ${userName}! \n`)
+  console.log(`You are currently in ${dirPath}`)
 
   const rl = readlinePromises.createInterface({
     input: process.stdin,
@@ -46,8 +58,8 @@ const initFileManagerApp = async () => {
   })
 
   rl.on(`line`, async (line) => {
-    await handleUserInput(line, currentDirectory)
-    console.log(`You are currently in ${currentDirectory}`)
+    await handleUserInput(line, dirPath)
+    console.log(`You are currently in ${dirPath}`)
   })
 
   rl.on('close', () => {
